4cb7266731198de83bf047c2a1c52934875ccd7f
Brandon Askea <brandon.askea@me.com>
2018-11-05 13:21:25 -0800

    Initial Commit

---
+++ RRStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RRStream
+//
+//  Created by Brandon Askea on 11/5/18.
+//  Copyright © 2018 Brandon Askea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RRStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RRStream
+//
+//  Created by Brandon Askea on 11/5/18.
+//  Copyright © 2018 Brandon Askea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
